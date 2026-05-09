7f8fe2f453431b60abbbddc88cc2db36cf584541
mteranc <mteranc@unemi.edu.ec>
2026-05-09 11:33:15 -0500

    changes 4

--- backend/src/interfaces/express.d.ts
+++ backend/src/interfaces/express.d.ts
@@ -1,24 +1,22 @@
-export {};
+import 'express';
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        id: string;
-        nombres: string;
-        apellidos: string;
-        email: string;
-        username: string;
-        password: string;
-        imagen: string | null;
-        estado_user: 'online' | 'offline' | 'away' | 'busy';
-        biografia: string | null;
-        createdAt: Date;
-        updatedAt: Date | null;
-        status: boolean;
-      };
-      currentFiles?: Record<string, string | null>;
-      fileResult?: Record<string, string | null>;
-    }
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: {
+      id: string;
+      nombres: string;
+      apellidos: string;
+      email: string;
+      username: string;
+      password: string;
+      imagen: string | null;
+      estado_user: 'online' | 'offline' | 'away' | 'busy';
+      biografia: string | null;
+      createdAt: Date;
+      updatedAt: Date | null;
+      status: boolean;
+    };
+    currentFiles?: Record<string, string | null>;
+    fileResult?: Record<string, string | null>;
   }
 }
