1227db2efa04c55eb50ec43edbaf0813fbed753a
mteranc <mteranc@unemi.edu.ec>
2026-05-09 12:52:05 -0500

    changes 6

--- backend/src/interfaces/express.d.ts
+++ backend/src/interfaces/express.d.ts
@@ -1,22 +1,16 @@
-import 'express';
+// src/types/express.d.ts
 
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: {
-      id: string;
-      nombres: string;
-      apellidos: string;
-      email: string;
-      username: string;
-      password: string;
-      imagen: string | null;
-      estado_user: 'online' | 'offline' | 'away' | 'busy';
-      biografia: string | null;
-      createdAt: Date;
-      updatedAt: Date | null;
-      status: boolean;
-    };
-    currentFiles?: Record<string, string | null>;
-    fileResult?: Record<string, string | null>;
+import { Usuario } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      // authenticacion
+      user?: Usuario;
+      // archivos
+      files?: Record<string, import('multer').File[]> | import('multer').File[];
+      currentFiles?: Record<string, string | null>;
+      fileResult?: Record<string, string | null>;
+    }
   }
 }
